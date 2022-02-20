2749582723c1416f3234893dc927ec2bd48b51c1
Liaquat Ali <ali.uibgmail.com>
2022-02-20 06:50:01 +0100

    git issue due to the provider exe

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
 // provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.97.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+// create resource group 
+resource "azurerm_resource_group" "azdevops" {
+  name     = "azdevops"
+  location = "West Europe"
+}
